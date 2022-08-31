16551e88826fff2f423088aeb783810026cea2ff
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-31 20:27:33 -0300

    118 - Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ cargarEventListeners();
 
 function cargarEventListeners() {
 
-    // Cuando clickeas en "agregar al carrito" sobre un curso.
+    // Cuando clickeo el botón "AGREGAR AL CARRITO"
     listaCursos.addEventListener('click', agregarCurso);
 
 }
@@ -17,8 +17,31 @@ function cargarEventListeners() {
 // Funciones
 
 function agregarCurso(e) {
+
     e.preventDefault()
+
     if (e.target.classList.contains('agregar-carrito')) {
-        console.log(e.target)
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+
+}
+
+// Tomar la información del curso que clickeamos
+
+function leerDatosCurso (curso) {
+
+    console.log(curso)
+
+    // Creo un objeto con el contenido del curso seleccionado
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'), // Obtengo el atributo data-id del elemento a que está dentro del elemento curso (.card)
+        cantidad: 1
+    }
+
+    console.log(infoCurso)
+
 }
